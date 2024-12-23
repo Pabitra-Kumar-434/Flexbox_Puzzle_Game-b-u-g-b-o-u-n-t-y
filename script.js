7a6f64250678fcd605d8457df4a5041d3c05859c
edith <mrutyunjaya.plb.od@gmail.com>
2024-12-23 11:25:33 +0530

    ----'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const $Game_levels = [
     {level:"1: Row Alignment",
      instructions:"Welcome to Flexbox Puzzle, a game where you help box by writing CSS code! Change the <code>flex-direction</code> property to arrange the boxes in a column instead of the default row layout. Experiment with horizontal layouts. ",
-     solution:"flex_direction:column;" 
+     solution:"flex-direction:column;" 
     },
     {level:"2: Align the Box",
      instructions:" To align the box horizontally within the container, use the <code>justify-content</code> property. Try <code>justify-content: center</code> to move the box to the middle. Experiment with other values like <code>flex-start</code> to align it to the left or <code>flex-end</code> to align it to the right. Observe how this property controls horizontal placement.",
@@ -45,7 +45,7 @@ const interface = document.getElementById('all');
 const nextBtn = document.getElementById('next');
 
 function interface_create(level_index){
-    const x = $Game_levels[level_index]
+    let x = $Game_levels[level_index]
     interface.innerHTML = `<div class="game">
         <div class="display"> 
             <div class="display-0" id="display-0">
@@ -57,8 +57,8 @@ function interface_create(level_index){
         <!-- Center Part -->
         <div class="center-part">
             <div class="code">
-                <h1 class="level">Level - <span>${$Game_levels.level}</span></h1>
-                <p>${$Game_levels.instructions}</p>
+                <h1 class="level">Level - <span>${x.level}</span></h1>
+                <p>${x.instructions}</p>
                 </div>
             <div class="coding">
                 <div class="lines">
@@ -71,41 +71,32 @@ function interface_create(level_index){
                         <li>6</li>
                         <li>7</li>                        
                     </ul>
-                    <pre class="coding-pre">#pond {
-        display: flex;
+        <pre class="coding-pre">#pond {
+display: flex;
         <textarea name="code" id="textarea" cols="30" rows="2"></textarea>
       }
                 </pre>
-                </div>
-                <div class="next">
-                    <button id="next">Next</button>
                 </div>
             </div>
         </div>
     </div>`;
 }
+const textarea = document.getElementById('textarea');
 
 function interface_change(style_input){
     const box_place = document.getElementById('display-0')
     box_place.style.cssText += style_input    
 }
 
-function animation(){
-    const boxes = document.querySelector('.box1');
-            boxes.forEach((box, index) => {
-                box.style.transform = translateX($`index * 100px`);
-                box.style.opacity = '0.8';
-            })
-            const boxs = document.querySelector('.box2');
-            boxes.forEach((box, index) => {
-                box.style.transform = translateX($`index * 100px`);
-                box.style.opacity = '0.8';
-            })
-            const boxe = document.querySelector('.box3');
-            boxes.forEach((box, index) => {
-                box.style.transform = translateX($`index * 100px`);
-                box.style.opacity = '0.8';
-            })
+function animation() {
+    const boxClasses = ['.box1', '.box2', '.box3'];
+    boxClasses.forEach(boxClass => {
+        const boxes = document.querySelectorAll(boxClass);
+        boxes.forEach((box, index) => {
+            box.style.transform = `translateX(${index * 100}px)`;
+            box.style.opacity = '0.8';
+        });
+    });
 }
 
 function level_check(){
@@ -113,21 +104,20 @@ function level_check(){
     const solution = $Game_levels[currentLevel].solution;
 
     if (userInput === solution) {
-        interface_change(userInput);
-        animation();
-        setTimeout(() => {
-            alert('Correct! Moving to the next level.');
-            currentLevel++;
-            if (currentLevel < $Game_levels.length) {
-                loadLevel(currentLevel);
-            } else {
-                alert('Congratulations! You completed all levels!');
-                submitBtn.disabled = true;
-            }
-        },1000);
+        interface_change(userInput); 
+        animation(); 
+        currentLevel++
+    
+        if (currentLevel < $Game_levels.length) {
+            interface_create(currentLevel); 
+        } else {
+            alert('Congratulations! You completed all levels!'); 
+            nextBtn.disabled = true; 
+        }
     } else {
-        alert('Incorrect! Try again.');
+        alert('Incorrect! Try again.'); 
     }
+    
 }
 
 interface_create(currentLevel)
